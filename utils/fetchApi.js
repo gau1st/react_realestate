7b44824e838d93ecf10a606ee612437db583685a
Gilang Esha Gautama <gilanggautama@bluprin.com>
2021-11-27 18:20:26 +0700

    hide rapid key api

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '1bf2b8308bmsha79378418794720p1fc92djsn35940e9348e4'
+            'x-rapidapi-key': 'process.env.NEXT_PUBLIC_RAPID_API_KEY'
         }
     })
 
